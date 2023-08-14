3a6b0ccd91ce4414c3b7c4aee66b8be27324ff12
eldar69 <somtooedeh26@gmail.com>
2023-08-14 12:41:39 +0100

    simple calculator

---
+++ index.js
@@ -0,0 +1,47 @@
+//to show something on the caculator screen
+function addToDisplay(value) {
+  const display = document.querySelector('.display');
+  display.value += value;
+}
+
+//to clear the display
+function clearDisplay() {
+  const display = document.querySelector('.display');
+  display.value = '';
+}
+
+//calculate
+function calculate() {
+  const display = document.querySelector('.display');
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
+
+function percentage() {
+  const display = document.querySelector('.display');
+  display.value = display.value / 100;
+}
+
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (button.textContent === 'C') {
+      clearDisplay();
+    } else if (button.textContent === '=') {
+      calculate();
+    } else if(button.textContent === '%'){
+      percentage();
+    }
+    else {
+      addToDisplay(button.textContent);
+    }
+  });
+});
+
+
+
